42528cedbd02a95714a02fde14c882e3cf6ad2ca
Sumiya Bano Samiullah <sumiyabanosamiullah@gmail.com>
2025-02-24 20:43:15 +0500

    Add files via upload

---
+++ Guess the number.py
@@ -0,0 +1,18 @@
+import random
+
+target=random.randint(1,100)
+while True:
+    userChoice=input("Guess the number or Quit(Q):")
+    if(userChoice=="Q"):
+        break
+    userChoice=int(userChoice)
+    if(userChoice==target):
+        print("Success:Correct Guess!!")
+        break
+    elif(userChoice<target):
+        print("Your number was too small.Take a bigger guess...")
+    else:
+        print("Your number was too big.Take a smaller guess...")
+
+print("____Game Over____")
+
